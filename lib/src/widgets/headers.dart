82c51d8784bb07ff29ca902d775bbc883d3b83f1
Martin Villanueva <fcovillanueva1988@gmail.com>
2023-01-17 14:40:10 -0600

    14. Header Circular

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -11,3 +11,21 @@ class SquareHeader extends StatelessWidget {
     );
   }
 }
+
+class RoundedEdgesHeader extends StatelessWidget {
+  const RoundedEdgesHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: const BoxDecoration(
+        color: Color(0xff615AAB),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+      ),
+    );
+  }
+}
