fe15667e37145f1ddeca749a66f2dcfdd76349ab
Martin Villanueva <fcovillanueva1988@gmail.com>
2023-01-17 15:11:59 -0600

    17. Header Diagonal

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -29,3 +29,43 @@ class RoundedEdgesHeader extends StatelessWidget {
     );
   }
 }
+
+class DiagonalHeader extends StatelessWidget {
+  const DiagonalHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _DiagonalHeaderPainter(),
+      ),
+    );
+  }
+}
+
+class _DiagonalHeaderPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final pencil = Paint();
+    //Properties
+    pencil.color = const Color(0xff615AAB);
+    pencil.style = PaintingStyle.fill;
+    pencil.strokeWidth = 5;
+
+    final path = Path();
+    //Draw with path and pencil
+    path.moveTo(0, 0);
+    path.lineTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, pencil);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
