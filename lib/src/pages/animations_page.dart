352ef45aa01bb459883ae323ce57727a67cadb72
Martin Villanueva <fcovillanueva1988@gmail.com>
2023-01-19 16:01:21 -0600

    29. Animation y AnimationController

--- lib/src/pages/animations_page.dart
+++ lib/src/pages/animations_page.dart
@@ -5,7 +5,7 @@ class AnimationsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: AnimatedSquare(),
       ),
@@ -20,10 +20,42 @@ class AnimatedSquare extends StatefulWidget {
   State<AnimatedSquare> createState() => _AnimatedSquareState();
 }
 
-class _AnimatedSquareState extends State<AnimatedSquare> {
+class _AnimatedSquareState extends State<AnimatedSquare>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+
+  late Animation<double> rotation;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 40000),
+    );
+    rotation = Tween(begin: 0.0, end: 2.0).animate(controller);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return _Rectangle();
+    controller.forward();
+
+    return AnimatedBuilder(
+      animation: controller,
+      child: _Rectangle(),
+      builder: (BuildContext context, Widget? child) {
+        return Transform.rotate(
+          angle: rotation.value,
+          child: child,
+        );
+      },
+    );
   }
 }
 
