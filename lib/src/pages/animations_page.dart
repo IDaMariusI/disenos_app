9a5a6af877b4878b21bb89cdf2a2065041857296
Martin Villanueva <fcovillanueva1988@gmail.com>
2023-01-20 08:55:54 -0600

    31. Curves

--- lib/src/pages/animations_page.dart
+++ lib/src/pages/animations_page.dart
@@ -33,11 +33,14 @@ class _AnimatedSquareState extends State<AnimatedSquare>
       vsync: this,
       duration: const Duration(milliseconds: 40000),
     );
-    rotation = Tween(begin: 0.0, end: 2 * Math.pi).animate(controller);
+
+    rotation = Tween(begin: 0.0, end: 2 * Math.pi).animate(
+      CurvedAnimation(parent: controller, curve: Curves.easeOut),
+    );
 
     controller.addListener(() {
       if (controller.status == AnimationStatus.completed) {
-        controller.reverse();
+        controller.reset();
       }
     });
 
