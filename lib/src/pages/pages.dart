1e3a1083784f789f449045ca8f6cb9bf41f6d059
IDaMariusI <fcovillanueva1988@gmail.com>
2023-02-10 15:46:39 -0600

    44. Preparacion del espacio para trabajar

--- lib/src/pages/pages.dart
+++ lib/src/pages/pages.dart
@@ -1,3 +1,4 @@
 export 'package:disenos_app/src/challenges/animated_square_page.dart';
 export 'package:disenos_app/src/pages/animations_page.dart';
+export 'package:disenos_app/src/pages/circular_progress_page.dart';
 export 'package:disenos_app/src/pages/headers_page.dart';
