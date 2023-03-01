205eca77d1d9d907b59038222830af25c5b0418f
IDaMariusI <fcovillanueva1988@gmail.com>
2023-03-01 11:38:31 -0600

    60. Inicio del espacio para trabajar

--- lib/src/pages/pages.dart
+++ lib/src/pages/pages.dart
@@ -2,3 +2,4 @@ export 'package:disenos_app/src/challenges/animated_square_page.dart';
 export 'package:disenos_app/src/pages/animations_page.dart';
 export 'package:disenos_app/src/pages/circle_graphs_page.dart';
 export 'package:disenos_app/src/pages/headers_page.dart';
+export 'package:disenos_app/src/pages/slideshow_page.dart';
