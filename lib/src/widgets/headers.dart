a9fd4691f83fef28ce84cc6717f5be1ae66de286
IDaMariusI <fcovillanueva1988@gmail.com>
2023-03-10 15:56:06 -0600

    97. Iconos y textos de mi IconHeader

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 class SquareHeader extends StatelessWidget {
   const SquareHeader({super.key});
 
@@ -297,6 +299,55 @@ class _WavesGradientHeaderPainter extends CustomPainter {
 class IconHeader extends StatelessWidget {
   const IconHeader({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    final Color whiteColor = Colors.white.withOpacity(0.7);
+
+    return Stack(
+      children: <Widget>[
+        _IconHeaderBackground(),
+        Positioned(
+          top: -50,
+          left: -70,
+          child: FaIcon(
+            FontAwesomeIcons.plus,
+            size: 200,
+            color: Colors.white.withOpacity(0.2),
+          ),
+        ),
+        Column(
+          children: <Widget>[
+            const SizedBox(height: 80, width: double.infinity),
+            Text(
+              'Haz solicitado',
+              style: TextStyle(
+                fontSize: 20,
+                color: whiteColor,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Asistencia Médica',
+              style: TextStyle(
+                fontSize: 25,
+                color: whiteColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const FaIcon(
+              FontAwesomeIcons.plus,
+              size: 80,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _IconHeaderBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
