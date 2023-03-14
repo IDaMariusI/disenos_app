23a69cae74cc946bdf878cf8f952f507f2fd1ba4
IDaMariusI <fcovillanueva1988@gmail.com>
2023-03-14 12:41:53 -0600

    110. Diseno basico de la applicacion

--- lib/src/pages/sliver_list_page.dart
+++ lib/src/pages/sliver_list_page.dart
@@ -5,9 +5,82 @@ class SliverListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('SliverListPage'),
+    return Scaffold(
+      body: _Title(),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        const SizedBox(height: 30),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+          child: const Text(
+            'New',
+            style: TextStyle(color: Color(0xff532128), fontSize: 50),
+          ),
+        ),
+        Stack(
+          children: <Widget>[
+            const SizedBox(width: 100),
+            Positioned(
+              bottom: 8,
+              child: Container(
+                width: 120,
+                height: 8,
+                color: const Color(0xffF7CDD5),
+              ),
+            ),
+            Container(
+              child: const Text(
+                'List',
+                style: TextStyle(
+                  color: Color(0xffD93A30),
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _ThingsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (BuildContext context, int index) => _ListItem(),
+    );
+  }
+}
+
+class _ListItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: const EdgeInsets.all(30),
+      height: 130,
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: const Text(
+        'Orangen',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
       ),
     );
   }
