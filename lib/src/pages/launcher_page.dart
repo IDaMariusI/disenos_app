5406e0ad259ebde5d17b494b263077bda903f7b8
IDaMariusI <fcovillanueva1988@gmail.com>
2023-03-17 14:44:14 -0600

    133. Rutas de nuestra aplicacion

--- lib/src/pages/launcher_page.dart
+++ lib/src/pages/launcher_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'package:disenos_app/src/routes/routes.dart';
+
 class LauncherPage extends StatelessWidget {
   const LauncherPage({super.key});
 
@@ -21,14 +23,19 @@ class _OptionsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
-      itemCount: 20,
+      itemCount: pageRoutes.length,
       physics: const BouncingScrollPhysics(),
       separatorBuilder: (context, index) => const Divider(color: Colors.blue),
       itemBuilder: (context, index) => ListTile(
-        leading: const FaIcon(FontAwesomeIcons.slideshare, color: Colors.blue),
-        title: const Text('Hola Mundo'),
+        leading: FaIcon(pageRoutes[index].icon, color: Colors.blue),
+        title: Text(pageRoutes[index].title),
         trailing: const Icon(Icons.chevron_right, color: Colors.blue),
-        onTap: () {},
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => pageRoutes[index].page),
+          );
+        },
       ),
     );
   }
---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:disenos_app/src/pages/pages.dart';
+
+final pageRoutes = <_Route>[
+  _Route(FontAwesomeIcons.slideshare, 'SlideShow', const SlideshowPage()),
+  _Route(FontAwesomeIcons.truckMedical, 'Emergency', const EmergencyPage()),
+  _Route(
+    FontAwesomeIcons.peopleCarryBox,
+    'Animated Square',
+    const AnimationsPage(),
+  ),
+  _Route(
+    FontAwesomeIcons.circleNotch,
+    'Progress Bar',
+    const CircleGraphsPage(),
+  ),
+  _Route(FontAwesomeIcons.pinterest, 'Pinterest', const PinterestPage()),
+  _Route(FontAwesomeIcons.mobile, 'Slivers', const SliverListPage()),
+];
+
+class _Route {
+  _Route(this.icon, this.title, this.page);
+
+  final IconData icon;
+  final String title;
+  final Widget page;
+}
